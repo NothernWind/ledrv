8f507238f46accacc0f38d44e77275e85f437e2c
NothernWind <kaljan.nothern@gmail.com>
2015-08-26 00:19:07 -0400

    0.0.0

--- ledrv.c
+++ ledrv.c
@@ -3,30 +3,28 @@
 #include <linux/kernel.h>
 #include <linux/gpio.h>
 
-static int gpio_pin = 26;
-static int gpio_value = 1;
-module_param(gpio_pin, int, 0755);
+static int led_gpio_pin = 26;
+//module_param(led_gpio_pin, int, 0755);
 static struct gpio led;
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kaljan");
 MODULE_DESCRIPTION("LED Driver. LED connected to GPIO26");
-//MODULE_SUPPORT_DEVICE("BlueLED");
 
 static int __init GPIO26_init(void)
 {
 	int gprq;
 	printk(KERN_NOTICE "gpio_init\n");
-	led.gpio = gpio_pin;
+	led.gpio = led_gpio_pin;
 	led.flags = GPIOF_OUT_INIT_LOW;
 	led.label = "Blue LED";
 	
-	if (!gpio_is_valid(gpio_pin)) {
-		printk(KERN_ALERT " GPIO number %d not valid.\n", gpio_pin);
+	if (!gpio_is_valid(led_gpio_pin)) {
+		printk(KERN_ALERT " GPIO number %d not valid.\n", led_gpio_pin);
 		return -1;
 	}
 	
-	gprq = gpio_request(gpio_pin,"Blue LED");
+	gprq = gpio_request(led_gpio_pin,"Blue LED");
 	
 	if (gprq) {
 		printk(KERN_ALERT "Gpio request failed.\n");
@@ -34,9 +32,9 @@ static int __init GPIO26_init(void)
     }
 	
 	printk(KERN_NOTICE "Set GPIO26 direction output\n");
-	gpio_direction_output(gpio_pin, 0);
+	gpio_direction_output(led_gpio_pin, 0);
 	printk(KERN_NOTICE "LED On\n");
-	gpio_set_value(gpio_pin, gpio_value);
+	gpio_set_value(led_gpio_pin, 1);
 	
 	return 0;
 }
@@ -44,7 +42,7 @@ static int __init GPIO26_init(void)
 static void __exit GPIO26_exit(void)
 {
 	printk(KERN_NOTICE "LED Off\n");
-	gpio_set_value(gpio_pin, 0);
+	gpio_set_value(led_gpio_pin, 0);
 	gpio_free(led.gpio);
 	printk(KERN_NOTICE "ldrv module exit\n");
 }
