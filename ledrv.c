b089dd8c83cc0dca82dcabcca313e1fa9d7eefd5
NothernWind <kaljan.nothern@gmail.com>
2015-08-25 22:34:48 -0400

    0.0.0

--- ledrv.c
+++ ledrv.c
@@ -15,19 +15,38 @@ MODULE_SUPPORT_DEVICE("BlueLED");
 
 static int __init GPIO26_init(void)
 {
+	int gpqr;
 	printk(KERN_NOTICE "gpio_init\n");
 	led.gpio = gpio_pin;
 	led.flags = GPIOF_OUT_INIT_LOW;
-	led.label = "LED";
+	led.label = "Blue LED";
 	
+	if (!gpio_is_valid(gpio_pin)) {
+		printk(KERN_ALERT " GPIO number %d not valid.\n", gpio_pin);
+		return -1;
+	}
 	
+	gprq = gpio_request(gpio_pin,"Blue LED");
+	
+	if (ret) {
+		printk(KERN_ALERT "Gpio request failed.\n");
+		return -2;
+    }
+	
+	printk(KERN_NOTICE "Set GPIO26 direction output\n");
+	gpio_direction_output(gpio_pin, 0);
+	printk(KERN_NOTICE "LED On\n");
+	gpio_set_value(gpio_pin, gpio_value);
 	
 	return 0;
 }
 
 static void __exit GPIO26_exit(void)
 {
-	gpio_deinit();
+	printk(KERN_NOTICE "LED Off\n");
+	gpio_set_value(gpio_pin, 0);
+	gpio_free(led.gpio);
+	printk(KERN_NOTICE "ldrv module exit\n");
 }
 
 module_init(GPIO26_init);
